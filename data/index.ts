0936fb1147f121c3eb869413209b690ab422cf12
Ibrahim Ahmed Siddiqui <107644916+Ibrahim-Siddiqui@users.noreply.github.com>
2024-12-15 18:14:09 +0500

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,308 @@
+export const navItems = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "Portfolio", link: "/portfolio" },
+  { name: "Contact", link: "/contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "We are very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Developing",
+    description: "We constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "InfineTeck delivers tailored software solutions.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Spotic Coin Blockchain website",
+    des: "Discover the future of digital currency with Spotic Coin – a blockchain-powered platform revolutionizing the way we transact.",
+    img: "/SpoticCoin-01 – 1.png",
+    iconLists: ["/eth.png", "/soli.png", "/tuf.png", "/next.svg", "/myt.png"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Candy Mall Mania - Roblox Game",
+    des: "Step into the sweet adventure of Candy Mall Mania – a delightful Roblox game filled with fun, challenges, and sugary surprises!",
+    img: "/candy.jpg",
+    iconLists: ["/rbs.png", "/bln.png", "/lua.png", "/maya.png", "/max.png"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Hide N Snipe - Roblox Game",
+    des: "Hide N Snipe – Dive into an immersive Roblox gaming experience blending strategy, stealth, and excitement like never before!",
+    img: "/hidnsnip.jpg",
+    iconLists: ["/rbs.png", "/bln.png", "/lua.png", "/maya.png", "/max.png"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "InnoVest - Investment Website 0.3",
+    des: "Experience the future of finance with InnoVest – an innovative investment platform powered by cutting-edge technology and seamless design.",
+    img: "/Web 1920 – 2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const faq = [
+  {
+    id: "0",
+    question: "How easy is it to setup Xora?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "1",
+    question: "Can I integrate Xora with other platforms?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "2",
+    question: "How often do you add new content?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "3",
+    question: "What your refund policy?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "4",
+    question: "Do you have corporate plans?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "5",
+    question: "What happens when I’m out of storage?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "6",
+    question: "Can I upgrade my plan?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "7",
+    question: "How do I invite my team?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "8",
+    question: "Do you offer training for individuals and teams?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "9",
+    question: "I’m locked out of my account what do I do?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "BlockChain Development",
+    desc: "Blockchain development builds trustless systems,transforming industries efficiency.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.png",
+  },
+  {
+    id: 2,
+    title: "Mobile App & Website Development",
+    desc: "Mobile app and website development deliver seamless digital solutions that enhance user experience & drive business growth.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.png",
+  },
+  {
+    id: 3,
+    title: "Metaverse Virtual Tours",
+    desc: "We offer immersive, interactive experiences that transform how users explore and engage with digital environments.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.png",
+  },
+  {
+    id: 4,
+    title: "Game Development",
+    desc: "As a software development company, we use Unity and Roblox to create immersive, interactive game worlds that captivate players and bring ideas to life.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.png",
+  },
+  {
+    id: 5,
+    title: "Digital Marketing & Branding",
+    desc: "Digital marketing and branding services help businesses establish a strong online presence.",
+    className: "md:col-span-2",
+    thumbnail: "/exp5.png",
+  },
+  {
+    id: 6,
+    title: "2D & 3D Animation - Video Editing",
+    desc: "2D and 3D animation and video editing services help you bring your vision to life.",
+    className: "md:col-span-2",
+    thumbnail: "/exp6.png",
+  },
+  {
+    id: 7,
+    title: "E-commerce Consultancy",
+    desc: "2D and 3D animation and video editing services help you bring your vision to life.",
+    className: "md:col-span-2",
+    thumbnail: "/exp7.png",
+  },
+  {
+    id: 8,
+    title: "Artificial Intelligence Development",
+    desc: "We develop AI for chatbots, LLM models, predictive analytics, automation, and personalized solutions that transform businesses.",
+    className: "md:col-span-2",
+    thumbnail: "/exp8.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
